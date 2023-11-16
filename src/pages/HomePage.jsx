bdbcd33043c64ab25adb223ca2e8946d35d6866e
Jonathan S <74302636+JaceJS@users.noreply.github.com>
2023-11-16 11:47:10 +0700

    fix: fix image link

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,14 @@ import React, { useEffect } from 'react'
 import Typed from 'typed.js'
 import { NavLink } from 'react-router-dom'
 import AnimateButton from '../components/AnimateButton'
+import HomeBackground from '../assets/images/home-background.jpg'
 
 const HomePage = () => {
   const el = React.useRef(null);
-
+  const BackgroundImage = {
+    backgroundImage: `url(${HomeBackground})`,
+  };
+  
   useEffect(() => {
     const typed = new Typed(el.current, {
       strings: ["Quality Assurance", "Software Tester"],
@@ -20,7 +24,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="h-screen w-full flex justify-center items-center bg-home-pattern bg-no-repeat bg-cover bg-[center_30px]">        
+    <div className="h-screen w-full flex justify-center items-center bg-no-repeat bg-cover bg-[center_30px]" style={BackgroundImage}>        
       <div className="flex flex-col justify-center items-center">        
 
         <div data-aos="fade-up" className="text-center p-3 mb-5 lg:mb-16 text-4xl md:text-6xl lg:text-8xl font-semibold overflow-hidden">
